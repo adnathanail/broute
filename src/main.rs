c2abb4d606a8cfa9006e7adfb3fbb24bea38acc6
Alex Nathanail <adnathanail@hotmail.co.uk>
2022-08-17 01:48:37 +0100

    Implement depth first search

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,27 @@ mod practice;
 mod graph;
 
 fn main() {
-    let mut g = graph::Graph::new(10);
+    let mut g = graph::Graph::new(13);
 
     println!("{:?}", g);
 
-    g.add_edge(1, 3);
+    g.add_edge(0, 1);
+    g.add_edge(0, 2);
+    g.add_edge(0, 5);
+    g.add_edge(0, 6);
+    g.add_edge(3, 4);
+    g.add_edge(3, 5);
+    g.add_edge(4, 5);
+    g.add_edge(4, 6);
+
+    g.add_edge(7, 8);
+
+    g.add_edge(9, 10);
+    g.add_edge(9, 11);
+    g.add_edge(9, 12);
+    g.add_edge(11, 12);
 
     println!("{:?}", g);
+
+    g.dfs();
 }
