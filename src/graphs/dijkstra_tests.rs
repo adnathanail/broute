4088af9509b21c5d15a5e79c5b9f3f8b6d763db3
Alex Nathanail <adnathanail@hotmail.co.uk>
2022-08-18 00:22:17 +0100

    Add Dijkstra test

---
+++ src/graphs/dijkstra_tests.rs
@@ -0,0 +1,25 @@
+use super::*;
+
+#[test]
+fn dijkstra_test() {
+    let mut g = digraph::Digraph::new(8);
+
+    g.add_edge(0, 1, 5.0);
+    g.add_edge(0, 4, 9.0);
+    g.add_edge(0, 7, 8.0);
+    g.add_edge(1, 2, 12.0);
+    g.add_edge(1, 3, 15.0);
+    g.add_edge(1, 7, 4.0);
+    g.add_edge(2, 3, 3.0);
+    g.add_edge(2, 6, 11.0);
+    g.add_edge(3, 6, 9.0);
+    g.add_edge(4, 5, 4.0);
+    g.add_edge(4, 6, 20.0);
+    g.add_edge(4, 7, 5.0);
+    g.add_edge(5, 2, 1.0);
+    g.add_edge(5, 6, 13.0);
+    g.add_edge(7, 2, 7.0);
+    g.add_edge(7, 5, 6.0);
+
+    assert_eq!(dijkstra(g), [0.0, 5.0, 14.0, 17.0, 9.0, 13.0, 25.0, 8.0]);
+}
