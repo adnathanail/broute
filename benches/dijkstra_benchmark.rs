2272cac09f1ecebfd7bc4a4ea148c044dcdabe73
Alex Nathanail <adnathanail@hotmail.co.uk>
2022-08-20 02:28:47 +0100

    Create benchmark for Dijkstra algorithm

---
+++ benches/dijkstra_benchmark.rs
@@ -0,0 +1,28 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use broute::graphs;
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let mut g = graphs::digraph::Digraph::new(8);
+
+    g.add_edge(0, 1, 5.0);
+    g.add_edge(0, 4, 9.0);
+    g.add_edge(0, 7, 8.0);
+    g.add_edge(1, 2, 12.0);
+    g.add_edge(1, 3, 15.0);
+    g.add_edge(1, 7, 4.0);
+    g.add_edge(2, 3, 3.0);
+    g.add_edge(2, 6, 11.0);
+    g.add_edge(3, 6, 9.0);
+    g.add_edge(4, 5, 4.0);
+    g.add_edge(4, 6, 20.0);
+    g.add_edge(4, 7, 5.0);
+    g.add_edge(5, 2, 1.0);
+    g.add_edge(5, 6, 13.0);
+    g.add_edge(7, 2, 7.0);
+    g.add_edge(7, 5, 6.0);
+
+    c.bench_function("Dijkstra", |b| b.iter(|| graphs::dijkstra::dijkstra(black_box(&g))));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
