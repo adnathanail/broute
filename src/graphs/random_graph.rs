6f3ce65120517bbd7aa93b3c14cecf09668ee1dd
Alex Nathanail <adnathanail@hotmail.co.uk>
2022-09-02 02:59:44 +0100

    Fix lints

--- src/graphs/random_graph.rs
+++ src/graphs/random_graph.rs
@@ -16,15 +16,13 @@ pub fn get_random_graph(
 
     for i in 0..num_nodes {
         for j in 0..num_nodes {
-            if i != j {
-                if (&mut rng).gen::<f32>() > conn_prob {
-                    let raw_weight = normal.sample(&mut rng);
-                    let rounded_weight = (raw_weight * 100.0).round() / 100.0;
-                    g.add_edge(i, j, rounded_weight);
-                }
+            if i != j && (&mut rng).gen::<f32>() > conn_prob {
+                let raw_weight = normal.sample(&mut rng);
+                let rounded_weight = (raw_weight * 100.0).round() / 100.0;
+                g.add_edge(i, j, rounded_weight);
             }
         }
     }
 
-    return g;
+    g
 }
