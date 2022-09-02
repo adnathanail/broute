371aa0b6318402c0ba11a9924ccf148e833b234e
Alex Nathanail <adnathanail@hotmail.co.uk>
2022-09-02 02:54:10 +0100

    Move dijkstra2 into dijkstra module

--- src/graphs/dijkstra.rs
+++ src/graphs/dijkstra.rs
@@ -1,6 +1,11 @@
 use std::collections::VecDeque;
+use super::super::algorithms::priority_queue::PriorityQueue;
 
 use super::digraph;
+
+#[cfg(test)]
+#[path = "dijkstra_tests.rs"]
+mod dijkstra_tests;
 
 pub fn dijkstra(g: &digraph::Digraph) -> Vec<f32> {
     // Initialise all distances to infinity
@@ -33,6 +38,33 @@ pub fn dijkstra(g: &digraph::Digraph) -> Vec<f32> {
         for u in g.adj(v) {
             let alt = dist_to[v] + u.weight;
             if alt < dist_to[u.to] {
+                dist_to[u.to] = alt;
+            }
+        }
+    }
+    dist_to
+}
+
+pub fn dijkstra2(g: &digraph::Digraph) -> Vec<f32> {
+    // Initialise all distances to infinity
+    let mut dist_to = vec![f32::INFINITY; g.num_vertices as usize];
+    dist_to[0] = 0.0;
+    // Add first vertex to queue
+    let mut queue = PriorityQueue::new();
+    queue.push(0.0, 0);
+
+    // Take next closest node from the heap
+    while let Some((v, cost)) = queue.pop() {
+        // Short circuit
+        if cost > dist_to[v] { continue; }
+
+        // Check every node, u, reachable from v
+        //   to see if a route via v is shorter than the current shortest path
+        for u in g.adj(v) {
+            let alt = dist_to[v] + u.weight;
+            if alt < dist_to[u.to] {
+                // Add adjacent node to queue
+                queue.push(alt, u.to);
                 dist_to[u.to] = alt;
             }
         }
