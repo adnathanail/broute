6305efeddb7af2eec6661b0ca5db98879d8b1cb6
Alex Nathanail <adnathanail@hotmail.co.uk>
2022-11-20 03:20:45 +0000

    Add support to read graph from tsplib file

--- src/graphs/mod.rs
+++ src/graphs/mod.rs
@@ -3,3 +3,4 @@ pub mod dijkstra;
 pub mod random_graph;
 pub mod searches;
 pub mod output;
+pub mod tsplib;
---
+++ src/graphs/tsplib.rs
@@ -0,0 +1,28 @@
+use tsplib::{EdgeWeightType, NodeCoord, Type};
+use std::io::Cursor;
+
+use super::digraph::Digraph;
+
+pub fn load_tsplib_file(input_data: &[u8]) -> Digraph {
+    let instance = tsplib::parse(Cursor::new(&input_data[..])).unwrap();
+
+    let coords = match instance.node_coord.unwrap() {
+        NodeCoord::Two(x) => x,
+        _ => panic!("Wrong format")
+    };
+
+    let mut g = Digraph::new(10);
+
+    for coord1 in coords.iter() {
+        for coord2 in coords.iter() {
+            if coord1.0 != coord2.0 {
+                let dx = coord2.1 - coord1.1;
+                let dy = coord2.2 - coord1.2;
+                let weight = (f32::powf(dx, 2.0) + f32::powf(dy, 2.0)).sqrt();
+                g.add_edge(coord1.0, coord2.0, weight)
+            }
+        }
+    }
+
+    g
+}
