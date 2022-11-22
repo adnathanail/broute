4b541320d41c892fac6a658c85b9eea7a4e52823
Alex Nathanail <adnathanail@hotmail.co.uk>
2022-11-22 21:44:14 +0000

    Fix TSP loading

--- src/graphs/tsplib.rs
+++ src/graphs/tsplib.rs
@@ -3,7 +3,7 @@ use std::io::Cursor;
 
 use super::digraph::Digraph;
 
-pub fn load_tsplib_file(input_data: &[u8]) -> Digraph {
+pub fn load_tsplib_file(input_data: String) -> Digraph {
     let instance = tsplib::parse(Cursor::new(&input_data[..])).unwrap();
 
     let coords = match instance.node_coord.unwrap() {
@@ -11,7 +11,7 @@ pub fn load_tsplib_file(input_data: &[u8]) -> Digraph {
         _ => panic!("Wrong format")
     };
 
-    let mut g = Digraph::new(10);
+    let mut g = Digraph::new(instance.dimension);
 
     for coord1 in coords.iter() {
         for coord2 in coords.iter() {
@@ -19,7 +19,7 @@ pub fn load_tsplib_file(input_data: &[u8]) -> Digraph {
                 let dx = coord2.1 - coord1.1;
                 let dy = coord2.2 - coord1.2;
                 let weight = (f32::powf(dx, 2.0) + f32::powf(dy, 2.0)).sqrt();
-                g.add_edge(coord1.0, coord2.0, weight)
+                g.add_edge(coord1.0 - 1, coord2.0 - 1, weight)  // -1 because TSPLIB is 1 indexed
             }
         }
     }
