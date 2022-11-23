3926e33960194834b7abfd8632e1f823f88af6b2
Alex Nathanail <adnathanail@hotmail.co.uk>
2022-11-23 01:44:19 +0000

    Lay out the bones of the TSP solver

--- src/main.rs
+++ src/main.rs
@@ -3,34 +3,24 @@
 
 use std::fs;
 
-use broute::graphs::{tsplib::load_tsplib_file, output::{output_graph_to_file, output_graph_to_file_with_path}, dijkstra::dijkstra, travelling_salesman::{travelling_salesman, get_path_length}};
+use broute::graphs::{tsplib::load_tsplib_file, output::{output_graph_to_file_with_path}, travelling_salesman::{travelling_salesman, get_path_length}};
 
 fn main() {
     let tsp_string = fs::read_to_string("test_data/dimacs_tsp/d1291.tsp").unwrap();
 
     println!("Reading file");
 
-    let g = load_tsplib_file(tsp_string, 5);
-
-    output_graph_to_file(&g, "out/graph.svg".to_string());
-
-    println!("Running Dijkstra");
-
-    println!("{:?}", dijkstra(&g));
+    let g = load_tsplib_file(tsp_string, 10);
 
     println!("Solving travelling salesman");
 
     let path = travelling_salesman(&g);
 
-    println!("{:?}", path);
+    println!("Final");
 
-    println!("Getting path length");
-
-    println!("{}", get_path_length(&g, &path));
-
-    println!("Visualising path");
-
-    output_graph_to_file_with_path(&g, &path, "out/path.svg".to_string());
+    println!("\t{:?}", path.path);
+    println!("\t{}", get_path_length(&g, &path));
+    output_graph_to_file_with_path(&g, &path, "out/path_final.svg".to_string());
 
     println!("Done!");
 }
