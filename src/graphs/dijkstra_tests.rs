1c12888f393b58f9f7105fb443dbc7ad8e935427
Alex Nathanail <adnathanail@hotmail.co.uk>
2023-01-07 21:42:19 +0000

    Fix lints

--- src/graphs/dijkstra_tests.rs
+++ src/graphs/dijkstra_tests.rs
@@ -1,30 +1,31 @@
 use super::*;
 use crate::graphs::datastructures;
+use crate::graphs::datastructures::digraph::NodeID;
 
 #[test]
 fn dijkstra_test() {
     let mut g = datastructures::am_digraph::AMDigraph::new(8);
 
     for i in 0..8 {
-        g.add_node_data(i, 0.0, 0.0);
+        g.add_node_data(NodeID(i), 0.0, 0.0);
     }
 
-    g.add_edge(0, 1, 5.0);
-    g.add_edge(0, 4, 9.0);
-    g.add_edge(0, 7, 8.0);
-    g.add_edge(1, 2, 12.0);
-    g.add_edge(1, 3, 15.0);
-    g.add_edge(1, 7, 4.0);
-    g.add_edge(2, 3, 3.0);
-    g.add_edge(2, 6, 11.0);
-    g.add_edge(3, 6, 9.0);
-    g.add_edge(4, 5, 4.0);
-    g.add_edge(4, 6, 20.0);
-    g.add_edge(4, 7, 5.0);
-    g.add_edge(5, 2, 1.0);
-    g.add_edge(5, 6, 13.0);
-    g.add_edge(7, 2, 7.0);
-    g.add_edge(7, 5, 6.0);
+    g.add_edge(NodeID(0), NodeID(1), 5.0);
+    g.add_edge(NodeID(0), NodeID(4), 9.0);
+    g.add_edge(NodeID(0), NodeID(7), 8.0);
+    g.add_edge(NodeID(1), NodeID(2), 12.0);
+    g.add_edge(NodeID(1), NodeID(3), 15.0);
+    g.add_edge(NodeID(1), NodeID(7), 4.0);
+    g.add_edge(NodeID(2), NodeID(3), 3.0);
+    g.add_edge(NodeID(2), NodeID(6), 11.0);
+    g.add_edge(NodeID(3), NodeID(6), 9.0);
+    g.add_edge(NodeID(4), NodeID(5), 4.0);
+    g.add_edge(NodeID(4), NodeID(6), 20.0);
+    g.add_edge(NodeID(4), NodeID(7), 5.0);
+    g.add_edge(NodeID(5), NodeID(2), 1.0);
+    g.add_edge(NodeID(5), NodeID(6), 13.0);
+    g.add_edge(NodeID(7), NodeID(2), 7.0);
+    g.add_edge(NodeID(7), NodeID(5), 6.0);
 
     assert_eq!(dijkstra(&g), [0.0, 5.0, 14.0, 17.0, 9.0, 13.0, 25.0, 8.0]);
     assert_eq!(dijkstra2(&g), [0.0, 5.0, 14.0, 17.0, 9.0, 13.0, 25.0, 8.0]);
