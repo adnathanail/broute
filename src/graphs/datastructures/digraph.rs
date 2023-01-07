9436246034cb7dfa506a62296b9b41fd87ff557c
Alex Nathanail <adnathanail@hotmail.co.uk>
2023-01-07 21:04:17 +0000

    Stop adj and dist using node_id

--- src/graphs/datastructures/digraph.rs
+++ src/graphs/datastructures/digraph.rs
@@ -1,6 +1,5 @@
 #[derive(Debug)]
 pub struct NodeData {
-    pub node_index: usize,
     pub longitude: f64,
     pub latitude: f64,
 }
@@ -13,13 +12,19 @@ pub struct DigraphAdjacency {
 pub trait Digraph {
     fn num_vertices(&self) -> usize;
 
+    // Graph creation and data reading functions accept a node_id, which can be any usize value, to
+    //   easily map OpenStreetMap node ID's onto my custom graph implementation
     fn add_node_data(&mut self, node_id: usize, longitude: f64, latitude: f64);
 
     fn add_edge(&mut self, from_id: usize, to_id: usize, weight: f64);
 
-    fn adj(&self, node_id: usize) -> Vec<DigraphAdjacency>;
-
-    fn dist(&self, from_id: usize, to_id: usize) -> f64;
-
     fn get_node_data(&self, node_id: usize) -> &NodeData;
+
+    // Translation between node_id and node_index is slow, so high-traffic functions (those called
+    //   in the actual algorithms themselves, instead of setup/result reading) work directly with
+    //   node_index
+    // This should allow the shortest path/travelling salesman code to never worry about node_id's
+    fn adj(&self, node_index: usize) -> Vec<DigraphAdjacency>;
+
+    fn dist(&self, from_index: usize, to_index: usize) -> f64;
 }
