cd24dc70e49be8433d67787549ed1dc5bc973fa7
Alex Nathanail <adnathanail@hotmail.co.uk>
2023-01-07 18:14:35 +0000

    Make benchmark comments better Closes #27

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -12,39 +12,39 @@ fn dijkstra_benchmark(c: &mut Criterion) {
     group.bench_with_input(BenchmarkId::new("v1", &g), &g, |b, g| {
         b.iter(|| graphs::dijkstra::dijkstra(g))
     });
-    group.bench_with_input(BenchmarkId::new("v2", &g), &g, |b, g| {
-        b.iter(|| graphs::dijkstra::dijkstra2(g))
-    });
+    // group.bench_with_input(BenchmarkId::new("v2", &g), &g, |b, g| {
+    //     b.iter(|| graphs::dijkstra::dijkstra2(g))
+    // });
 
     group.finish();
 
-    let tsp_string = fs::read_to_string("test_data/dimacs_tsp/d1291.tsp").unwrap();
-    let g = graphs::tsplib::load_tsplib_file(tsp_string, usize::max_value());
-
-    let mut group = c.benchmark_group("Dijkstra (DIMCAS d1291)");
-
-    group.bench_with_input(BenchmarkId::new("v1", &g), &g, |b, g| {
-        b.iter(|| graphs::dijkstra::dijkstra(g))
-    });
-    group.bench_with_input(BenchmarkId::new("v2", &g), &g, |b, g| {
-        b.iter(|| graphs::dijkstra::dijkstra2(g))
-    });
-
-    group.finish();
+    // let tsp_string = fs::read_to_string("test_data/dimacs_tsp/d1291.tsp").unwrap();
+    // let g = graphs::tsplib::load_tsplib_file(tsp_string, usize::max_value());
+    //
+    // let mut group = c.benchmark_group("Dijkstra (DIMCAS d1291)");
+    //
+    // group.bench_with_input(BenchmarkId::new("v1", &g), &g, |b, g| {
+    //     b.iter(|| graphs::dijkstra::dijkstra(g))
+    // });
+    // group.bench_with_input(BenchmarkId::new("v2", &g), &g, |b, g| {
+    //     b.iter(|| graphs::dijkstra::dijkstra2(g))
+    // });
+    //
+    // group.finish();
 }
 
-fn travelling_salesman_benchmark(c: &mut Criterion) {
-    let tsp_string = fs::read_to_string("test_data/dimacs_tsp/d1291.tsp").unwrap();
-    let g = graphs::tsplib::load_tsplib_file(tsp_string, usize::max_value());
-
-    let mut group = c.benchmark_group("Travelling salesman (DIMCAS d1291)");
-
-    group.bench_with_input(BenchmarkId::new("v1", &g), &g, |b, g| {
-        b.iter(|| graphs::travelling_salesman::travelling_salesman(g, false))
-    });
-
-    group.finish();
-}
+// fn travelling_salesman_benchmark(c: &mut Criterion) {
+//     let tsp_string = fs::read_to_string("test_data/dimacs_tsp/d1291.tsp").unwrap();
+//     let g = graphs::tsplib::load_tsplib_file(tsp_string, usize::max_value());
+//
+//     let mut group = c.benchmark_group("Travelling salesman (DIMCAS d1291)");
+//
+//     group.bench_with_input(BenchmarkId::new("v1", &g), &g, |b, g| {
+//         b.iter(|| graphs::travelling_salesman::travelling_salesman(g, false))
+//     });
+//
+//     group.finish();
+// }
 
 criterion_group! {
     name = benches;
