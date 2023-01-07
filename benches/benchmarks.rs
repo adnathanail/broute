1b26ffb6ecc2731d2bcfa0b3a6fe863b3751ffe5
Alex Nathanail <adnathanail@hotmail.co.uk>
2023-01-07 11:06:16 +0000

    Combine benchmarks into 1 file

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -4,7 +4,7 @@ use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 
 use broute::graphs;
 
-fn criterion_benchmark(c: &mut Criterion) {
+fn dijkstra_benchmark(c: &mut Criterion) {
     let g = graphs::random_graph::get_random_graph(3000, 0.5, 4.0, 1.0);
 
     let mut group = c.benchmark_group("Dijkstra (random graph)");
@@ -33,10 +33,23 @@ fn criterion_benchmark(c: &mut Criterion) {
     group.finish();
 }
 
+fn travelling_salesman_benchmark(c: &mut Criterion) {
+    let tsp_string = fs::read_to_string("test_data/dimacs_tsp/d1291.tsp").unwrap();
+    let g = graphs::tsplib::load_tsplib_file(tsp_string, usize::max_value());
+
+    let mut group = c.benchmark_group("Travelling salesman (DIMCAS d1291)");
+
+    group.bench_with_input(BenchmarkId::new("v1", &g), &g, |b, g| {
+        b.iter(|| graphs::travelling_salesman::travelling_salesman(g))
+    });
+
+    group.finish();
+}
+
 criterion_group! {
     name = benches;
     // This can be any expression that returns a `Criterion` object.
     config = Criterion::default().measurement_time(Duration::from_secs(15));
-    targets = criterion_benchmark
+    targets = dijkstra_benchmark, travelling_salesman_benchmark
 }
 criterion_main!(benches);
