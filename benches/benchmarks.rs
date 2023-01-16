a6adcf9dfe515f7ec1854bb7995ef5542ce696ff
Alex Nathanail <adnathanail@hotmail.co.uk>
2023-01-16 11:31:45 +0000

    Fix lints

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -5,29 +5,29 @@ use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use broute::graphs;
 
 fn dijkstra_benchmark(c: &mut Criterion) {
-    let g = graphs::random_graph::get_random_graph(3000, 0.5, 4.0, 1.0);
+    let g = graphs::input::random_graph::get_random_graph(3000, 0.5, 4.0, 1.0);
 
     let mut group = c.benchmark_group("Dijkstra (random graph)");
 
     group.bench_with_input(BenchmarkId::new("v1", &g), &g, |b, g| {
-        b.iter(|| graphs::dijkstra::dijkstra(g))
+        b.iter(|| graphs::algorithms::dijkstra::dijkstra(g))
     });
     group.bench_with_input(BenchmarkId::new("v2", &g), &g, |b, g| {
-        b.iter(|| graphs::dijkstra::dijkstra2(g))
+        b.iter(|| graphs::algorithms::dijkstra::dijkstra2(g))
     });
 
     group.finish();
 
     let tsp_string = fs::read_to_string("test_data/dimacs_tsp/d1291.tsp").unwrap();
-    let g = graphs::tsplib::load_tsplib_file(tsp_string, usize::max_value());
+    let g = graphs::input::tsplib::load_tsplib_file(tsp_string, usize::MAX);
 
     let mut group = c.benchmark_group("Dijkstra (DIMCAS d1291)");
 
     group.bench_with_input(BenchmarkId::new("v1", &g), &g, |b, g| {
-        b.iter(|| graphs::dijkstra::dijkstra(g))
+        b.iter(|| graphs::algorithms::dijkstra::dijkstra(g))
     });
     group.bench_with_input(BenchmarkId::new("v2", &g), &g, |b, g| {
-        b.iter(|| graphs::dijkstra::dijkstra2(g))
+        b.iter(|| graphs::algorithms::dijkstra::dijkstra2(g))
     });
 
     group.finish();
@@ -35,12 +35,12 @@ fn dijkstra_benchmark(c: &mut Criterion) {
 
 fn travelling_salesman_benchmark(c: &mut Criterion) {
     let tsp_string = fs::read_to_string("test_data/dimacs_tsp/d1291.tsp").unwrap();
-    let g = graphs::tsplib::load_tsplib_file(tsp_string, usize::max_value());
+    let g = graphs::input::tsplib::load_tsplib_file(tsp_string, usize::MAX);
 
     let mut group = c.benchmark_group("Travelling salesman (DIMCAS d1291)");
 
     group.bench_with_input(BenchmarkId::new("v1", &g), &g, |b, g| {
-        b.iter(|| graphs::travelling_salesman::travelling_salesman(g, false))
+        b.iter(|| graphs::algorithms::travelling_salesman::travelling_salesman(g, false))
     });
 
     group.finish();
