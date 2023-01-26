b96f8d9896c4ac6a75b8ccdd2ce6dce973734f0f
Alex Nathanail <adnathanail@hotmail.co.uk>
2023-01-26 22:53:31 +0000

    Benchmark connected components

--- src/graphs/algorithms/connected_components.rs
+++ src/graphs/algorithms/connected_components.rs
@@ -1,5 +1,6 @@
 use std::cmp::min;
-use crate::graphs::datastructures::digraph::{Digraph, NodeIndex};
+use crate::graphs::datastructures::al_digraph::ALDigraph;
+use crate::graphs::datastructures::digraph::{Digraph, NodeID, NodeIndex};
 
 #[cfg(test)]
 #[path = "connected_components_tests.rs"]
@@ -16,6 +17,10 @@ pub struct ConnectedComponents<'a> {
 }
 
 impl<'a> ConnectedComponents<'a> {
+    /// Implements Tarjan's strongly connected component algorithm
+    ///
+    /// Original code based on pseudocode here
+    ///   https://en.wikipedia.org/wiki/Tarjan%27s_strongly_connected_components_algorithm#The_algorithm_in_pseudocode
     pub fn new(g: &'a dyn Digraph) -> Self {
         ConnectedComponents {
             g,
@@ -33,20 +38,6 @@ impl<'a> ConnectedComponents<'a> {
             if self.indexes[i] == -1 {
                 self.strong_connect(i);
             }
-        }
-
-        for component in 0..self.count {
-            let mut num_this_component = 0;
-            for i in 0..self.g.num_vertices() {
-                if self.cc[i] == component {
-                    num_this_component += 1;
-                }
-            }
-            println!("Component {:} Count {:}", component, num_this_component)
-        }
-        println!("{:}", self.count);
-        for i in 0..self.g.num_vertices() {
-            println!("ID {:} Component {:}", i, self.cc[i])
         }
     }
 
@@ -73,5 +64,42 @@ impl<'a> ConnectedComponents<'a> {
             }
             self.count += 1;
         }
+    }
+
+    pub fn get_connected_subgraphs(self, min_graph_size: usize) -> Vec<ALDigraph> {
+        let mut out = vec![];
+        for component in 0..self.count {
+            let mut num_this_component = 0;
+            let mut node_indexes_this_component: Vec<NodeIndex> = vec![];
+            for u in 0..self.g.num_vertices() {
+                if self.cc[u] == component {
+                    num_this_component += 1;
+                    node_indexes_this_component.push(NodeIndex(u))
+                }
+            }
+            if num_this_component < min_graph_size {
+                continue
+            }
+
+            let mut g = ALDigraph::new(num_this_component);
+            for u in &node_indexes_this_component {
+                let u_id = self.g.nodes_data().get_node_id_by_index(&u);
+                g.mut_nodes_data().add_node_data(
+                    *u_id,
+                    *self.g.nodes_data().get_node_data_by_index(*u)
+                );
+            }
+            for u in &node_indexes_this_component {
+                let u_id = self.g.nodes_data().get_node_id_by_index(&u);
+                for v in self.g.adj(*u) {
+                    let v_id = self.g.nodes_data().get_node_id_by_index(&NodeIndex(v.node_index.0));
+                    if node_indexes_this_component.contains(&v.node_index) {
+                        g.add_edge_by_id(*u_id, *v_id, v.weight)
+                    }
+                }
+            }
+            out.push(g)
+        }
+        out
     }
 }
