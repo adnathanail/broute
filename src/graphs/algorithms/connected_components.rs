b5dae647647da439bbecd9159c1d0ddf7234098d
Alex Nathanail <adnathanail@hotmail.co.uk>
2023-01-26 14:38:11 +0000

    Implement Tarjan's strongly connected component algorithm

--- src/graphs/algorithms/connected_components.rs
+++ src/graphs/algorithms/connected_components.rs
@@ -1,8 +1,16 @@
+use std::cmp::min;
 use crate::graphs::datastructures::digraph::{Digraph, NodeIndex};
+
+#[cfg(test)]
+#[path = "connected_components_tests.rs"]
+mod connected_components_tests;
 
 pub struct ConnectedComponents<'a> {
     g: &'a dyn Digraph,
-    marked: Vec<bool>,
+    index: i32,
+    node_stack: Vec<usize>,
+    indexes: Vec<i32>,
+    low_links: Vec<i32>,
     cc: Vec<i32>,
     count: i32,
 }
@@ -11,19 +19,22 @@ impl<'a> ConnectedComponents<'a> {
     pub fn new(g: &'a dyn Digraph) -> Self {
         ConnectedComponents {
             g,
-            marked: vec![false; g.num_vertices()],
+            index: 0,
+            node_stack: vec![],
+            indexes: vec![-1; g.num_vertices()],
+            low_links: vec![-1; g.num_vertices()],
+            count: 0,
             cc: vec![-1; g.num_vertices()],
-            count: 0
         }
     }
 
     pub fn run(&mut self) {
-        for v in 0..self.g.num_vertices() {
-            if !self.marked[v] {
-                self.cc_dfs(v);
-                self.count += 1;
+        for i in 0..self.g.num_vertices() {
+            if self.indexes[i] == -1 {
+                self.strong_connect(i);
             }
         }
+
         for component in 0..self.count {
             let mut num_this_component = 0;
             for i in 0..self.g.num_vertices() {
@@ -39,13 +50,28 @@ impl<'a> ConnectedComponents<'a> {
         }
     }
 
-    fn cc_dfs(&mut self, v: usize) {
-        self.marked[v] = true;
-        self.cc[v] = self.count;
+    fn strong_connect(&mut self, v: usize) {
+        self.indexes[v] = self.index;
+        self.low_links[v] = self.index;
+        self.index += 1;
+        self.node_stack.push(v);
+
         for w in self.g.adj(NodeIndex(v)) {
-            if !self.marked[w.node_index.0] {
-                self.cc_dfs(w.node_index.0);
+            if self.indexes[(&w).node_index.0] == -1 {
+                self.strong_connect((&w).node_index.0);
+                self.low_links[v] = min(self.low_links[v], self.low_links[(&w).node_index.0]);
+            } else if self.node_stack.contains(&(&w).node_index.0) {
+                self.low_links[v] = min(self.low_links[v], self.indexes[(&w).node_index.0])
             }
+        }
+
+        if self.low_links[v] == self.indexes[v] {
+            let mut w: i32 = -1;
+            while w != v as i32 {
+                w = self.node_stack.pop().unwrap() as i32;
+                self.cc[w as usize] = self.count;
+            }
+            self.count += 1;
         }
     }
 }
