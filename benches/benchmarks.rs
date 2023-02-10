b71121457b7e452892a2a430d6a41cea6030d30b
Alex Nathanail <adnathanail@hotmail.co.uk>
2023-02-10 00:20:46 +0000

    Benchmark both Dijkstra's on OSM Monaco

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -26,10 +26,29 @@ fn dijkstra_benchmark(c: &mut Criterion) {
 
     group.finish();
 
-    let tsp_string = fs::read_to_string("test_data/dimacs_tsp/d1291.tsp").unwrap();
-    let g = graphs::input::tsplib::load_tsplib_file(tsp_string, usize::MAX);
+    let g = graphs::input::tsplib::load_tsplib_file("test_data/dimacs_tsp/d1291.tsp", usize::MAX);
 
     let mut group = c.benchmark_group("Dijkstra (DIMCAS d1291)");
+
+    group.bench_with_input(BenchmarkId::new("v1", &g), &g, |b, g| {
+        b.iter(|| {
+            graphs::algorithms::dijkstra::dijkstra(g, graphs::datastructures::digraph::NodeIndex(0))
+        })
+    });
+    group.bench_with_input(BenchmarkId::new("v2", &g), &g, |b, g| {
+        b.iter(|| {
+            graphs::algorithms::dijkstra::dijkstra2(
+                g,
+                graphs::datastructures::digraph::NodeIndex(0),
+            )
+        })
+    });
+
+    group.finish();
+
+    let g = load_pbf_file("test_data/geofabrik/monaco-latest.osm.pbf");
+
+    let mut group = c.benchmark_group("Dijkstra (OSM Monaco)");
 
     group.bench_with_input(BenchmarkId::new("v1", &g), &g, |b, g| {
         b.iter(|| {
@@ -49,8 +68,7 @@ fn dijkstra_benchmark(c: &mut Criterion) {
 }
 
 fn travelling_salesman_benchmark(c: &mut Criterion) {
-    let tsp_string = fs::read_to_string("test_data/dimacs_tsp/d1291.tsp").unwrap();
-    let g = graphs::input::tsplib::load_tsplib_file(tsp_string, usize::MAX);
+    let g = graphs::input::tsplib::load_tsplib_file("test_data/dimacs_tsp/d1291.tsp", usize::MAX);
 
     let mut group = c.benchmark_group("Travelling salesman (DIMCAS d1291)");
 
