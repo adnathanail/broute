10ac6ef2112d476c992f9e36ac5928afd9ff52e4
Alex Nathanail <adnathanail@hotmail.co.uk>
2023-02-13 01:01:31 +0000

    New (quicker?) path length function

--- src/graphs/datastructures/graph_path.rs
+++ src/graphs/datastructures/graph_path.rs
@@ -7,8 +7,16 @@ pub struct GraphPath {
 
 impl GraphPath {
     pub fn get_length_on_graph(&self, g: &dyn Digraph) -> f64 {
-        (0..(self.path.len() - 1)).fold(0f64, |total, i| {
-            total + g.dist(self.path[i], self.path[i + 1])
+        let mut route_iter = self.path.iter();
+        let mut current_city = match route_iter.next() {
+            None => return 0.0,
+            Some(v) => *v,
+        };
+
+        route_iter.fold(0.0, |mut total_distance, &next_city| {
+            total_distance += g.dist(current_city, next_city);
+            current_city = next_city;
+            total_distance
         })
     }
 }
