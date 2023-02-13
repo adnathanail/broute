ceed04f3d1376d37efae3407a427cec50a2c3d4f
Alex Nathanail <adnathanail@hotmail.co.uk>
2023-02-13 00:25:10 +0000

    Create LatLng struct and update haversine Closes #70

--- src/algorithms/haversine.rs
+++ src/algorithms/haversine.rs
@@ -1,20 +1,14 @@
-pub fn haversine(
-    start_latitude: f64,
-    start_longitude: f64,
-    end_latitude: f64,
-    end_longitude: f64,
-) -> f64 {
+use crate::graphs::datastructures::LatLng;
+
+pub fn haversine(start_point: LatLng, end_point: LatLng) -> f64 {
     let earth_radius = 6371.0;
 
-    let start_latitude_radians = start_latitude.to_radians();
-    let end_latitude_radians = end_latitude.to_radians();
-
-    let delta_latitude_radians = (end_latitude - start_latitude).to_radians();
-    let delta_longitude_radians = (end_longitude - start_longitude).to_radians();
+    let delta_latitude_radians = (end_point.latitude - start_point.latitude).to_radians();
+    let delta_longitude_radians = (end_point.longitude - start_point.longitude).to_radians();
 
     let a = (delta_latitude_radians / 2.0).sin().powi(2)
-        + start_latitude_radians.cos()
-            * end_latitude_radians.cos()
+        + start_point.latitude_radians().cos()
+            * end_point.latitude_radians().cos()
             * (delta_longitude_radians / 2.0).sin().powi(2);
 
     let c = 2.0 * a.sqrt().atan2((1.0 - a).sqrt());
