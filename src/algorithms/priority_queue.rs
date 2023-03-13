05dd8c1ab061e60311d2817b98ef72e0f1039635
Alex Nathanail <adnathanail@hotmail.co.uk>
2023-03-13 14:32:50 +0000

    Genericise priority queue over priority Closes #95

--- src/algorithms/priority_queue.rs
+++ src/algorithms/priority_queue.rs
@@ -3,47 +3,47 @@ use std::cmp::Ordering::Equal;
 use std::collections::BinaryHeap;
 
 #[derive(Debug, Copy, Clone, PartialEq)]
-struct PriorityQueueItem<V: PartialEq> {
+struct PriorityQueueItem<V: PartialEq, P: PartialEq + PartialOrd> {
     value: V,
-    priority: f64,
+    priority: P,
 }
 
-impl<V: PartialEq> Eq for PriorityQueueItem<V> {}
+impl<V: PartialEq, P: PartialEq + PartialOrd> Eq for PriorityQueueItem<V, P> {}
 
 // The priority queue depends on `Ord`.
 // Explicitly implement the trait so the queue becomes a min-heap instead of a max-heap.
-impl<V: PartialEq> PartialOrd<Self> for PriorityQueueItem<V> {
+impl<V: PartialEq, P: PartialEq + PartialOrd> PartialOrd<Self> for PriorityQueueItem<V, P> {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         other.priority.partial_cmp(&self.priority)
     }
 }
 
-impl<V: PartialEq> Ord for PriorityQueueItem<V> {
+impl<V: PartialEq, P: PartialEq + PartialOrd> Ord for PriorityQueueItem<V, P> {
     fn cmp(&self, other: &Self) -> Ordering {
         self.partial_cmp(&other).unwrap_or(Equal)
     }
 }
 
 #[derive(Debug)]
-pub struct PriorityQueue<V: PartialEq> {
-    heap: BinaryHeap<PriorityQueueItem<V>>,
+pub struct PriorityQueue<V: PartialEq, P: PartialEq + PartialOrd> {
+    heap: BinaryHeap<PriorityQueueItem<V, P>>,
 }
 
-impl<V: PartialEq> PriorityQueue<V> {
+impl<V: PartialEq, P: PartialEq + PartialOrd> PriorityQueue<V, P> {
     pub fn new() -> Self {
         Self {
             heap: BinaryHeap::new(),
         }
     }
-    pub fn push(&mut self, value: V, priority: f64) {
+    pub fn push(&mut self, value: V, priority: P) {
         self.heap.push(PriorityQueueItem { value, priority })
     }
-    pub fn pop(&mut self) -> Option<(V, f64)> {
+    pub fn pop(&mut self) -> Option<(V, P)> {
         self.heap.pop().map(|s| (s.value, s.priority))
     }
 }
 
-impl<V: PartialEq> Default for PriorityQueue<V> {
+impl<V: PartialEq, P: PartialEq + PartialOrd> Default for PriorityQueue<V, P> {
     fn default() -> Self {
         Self::new()
     }
--- tests/algorithms/priority_queue_tests.rs
+++ tests/algorithms/priority_queue_tests.rs
@@ -2,7 +2,7 @@ use broute::algorithms::PriorityQueue;
 
 #[test]
 fn test_priority_queue() {
-    let mut queue = PriorityQueue::<usize>::new();
+    let mut queue = PriorityQueue::<usize, f64>::new();
     queue.push(1, 5.0);
     queue.push(2, 10.0);
     queue.push(3, 2.0);
@@ -12,10 +12,10 @@ fn test_priority_queue() {
 
 #[test]
 fn test_priority_queue_with_strings() {
-    let mut queue = PriorityQueue::<&str>::new();
-    queue.push("fish", 5.0);
-    queue.push("eggs", 10.0);
-    queue.push("bacon", 2.0);
+    let mut queue = PriorityQueue::<&str, usize>::new();
+    queue.push("fish", 20);
+    queue.push("eggs", 11);
+    queue.push("bacon", 55);
 
-    assert_eq!(queue.pop(), Some(("bacon", 2.0)));
+    assert_eq!(queue.pop(), Some(("eggs", 11)));
 }
