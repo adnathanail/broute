c210505dd5f3ab75cc7fa6ea6b346f20df3ce873
Alex Nathanail <adnathanail@hotmail.co.uk>
2023-03-21 14:44:30 +0000

    Create separate target for tune_sa

---
+++ src/bin/tune_sa.rs
@@ -0,0 +1,19 @@
+use std::time::SystemTime;
+use broute::graphs::algorithms::SimulatedAnnealing;
+use broute::graphs::input::load_tsplib_file;
+
+fn main() {
+    let dimacs_g = load_tsplib_file("test_data/dimacs_tsp/d1291.tsp", usize::MAX).unwrap();
+
+    for a in [0.9995] {
+        for i in [50, 100, 500, 1000] {
+            println!("100 {a} {i}");
+            let start = SystemTime::now();
+            let mut sa = SimulatedAnnealing::new(&dimacs_g);
+            sa.run();
+            let end = SystemTime::now();
+            let duration = end.duration_since(start).unwrap();
+            println!("\t{} seconds", duration.as_secs());
+        }
+    }
+}
