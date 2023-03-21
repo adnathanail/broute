5e2b21b4f8c0cadfc57d608f253064f0874821a2
Alex Nathanail <adnathanail@hotmail.co.uk>
2023-03-21 15:09:49 +0000

    More rebase fixes

--- tests/graphs/travelling_salesman_tests.rs
+++ tests/graphs/travelling_salesman_tests.rs
@@ -1,7 +1,7 @@
-use broute::graphs::algorithms::{form_abstracted_graph, ConnectedComponents, SimulatedAnnealing};
-use broute::graphs::datastructures::Digraph;
+use broute::geography::datastructures::LatLng;
+use broute::graphs::algorithms::{ConnectedComponents, form_abstracted_graph, SimulatedAnnealing};
+use broute::graphs::datastructures::{Digraph, NodeID, NodeIndex};
 use broute::graphs::input::{load_pbf_file, load_tsplib_file};
-use rand::seq::IteratorRandom;
 
 #[test]
 fn travelling_salesman_dimacs_test() {
@@ -11,19 +11,19 @@ fn travelling_salesman_dimacs_test() {
     sa.run(100.0, 0.99, 100);
 }
 
-// fn check_graph_adjacency(
-//     g: &impl Digraph,
-//     node_id: &NodeID,
-//     expected_adjacency: Vec<(NodeIndex, f64)>,
-// ) {
-//     let node_index = *g.nodes_data().get_node_index_by_id(node_id);
-//     let actual_adjacency: Vec<(NodeIndex, f64)> = g
-//         .adj(node_index)
-//         .into_iter()
-//         .map(|adjacency| (adjacency.node_index, adjacency.weight))
-//         .collect();
-//     assert_eq!(actual_adjacency, expected_adjacency);
-// }
+fn check_graph_adjacency(
+    g: &impl Digraph,
+    node_id: &NodeID,
+    expected_adjacency: Vec<(NodeIndex, f64)>,
+) {
+    let node_index = *g.nodes_data().get_node_index_by_id(node_id);
+    let actual_adjacency: Vec<(NodeIndex, f64)> = g
+        .adj(node_index)
+        .into_iter()
+        .map(|adjacency| (adjacency.node_index, adjacency.weight))
+        .collect();
+    assert_eq!(actual_adjacency, expected_adjacency);
+}
 
 #[test]
 fn a_star_travelling_salesman_integration_test() {
@@ -37,13 +37,99 @@ fn a_star_travelling_salesman_integration_test() {
     assert_eq!(c_g.num_vertices(), 9936);
 
     // 5 points across Monaco
-    let all_node_ids = c_g.nodes_data().get_node_ids();
-    let selected_node_ids = all_node_ids
-        .into_iter()
-        .choose_multiple(&mut rand::thread_rng(), 10);
+    let pickup_points: Vec<LatLng> = Vec::from([
+        LatLng {
+            latitude: 43.7290416923665,
+            longitude: 7.4111366271972665,
+        },
+        LatLng {
+            latitude: 43.732592386855366,
+            longitude: 7.416672706604005,
+        },
+        LatLng {
+            latitude: 43.727630659255674,
+            longitude: 7.419397830963136,
+        },
+        LatLng {
+            latitude: 43.732297795757404,
+            longitude: 7.425642013549806,
+        },
+        LatLng {
+            latitude: 43.739507109017445,
+            longitude: 7.42926836013794,
+        },
+    ]);
+
+    // Get ID of closest node for each point
+    let mut pickup_node_ids: Vec<NodeID> = vec![];
+    for lat_lng in pickup_points {
+        let node_index = c_g.nodes_data().get_node_index_closest_to_lat_lng(lat_lng);
+        let node_id = c_g.nodes_data().get_node_id_by_index(&node_index);
+        pickup_node_ids.push(*node_id);
+    }
+    assert_eq!(
+        pickup_node_ids,
+        [
+            NodeID(1074585047),
+            NodeID(252362113),
+            NodeID(1204303590),
+            NodeID(1573112159),
+            NodeID(1736929694)
+        ]
+    );
 
     // Form abstracted graph
-    let abstracted_graph = form_abstracted_graph(&c_g, &selected_node_ids);
+    let abstracted_graph = form_abstracted_graph(&c_g, &pickup_node_ids);
+    check_graph_adjacency(
+        &abstracted_graph,
+        &pickup_node_ids[0],
+        vec![
+            (NodeIndex(1), 1.377158916250499),
+            (NodeIndex(2), 0.9789951317313738),
+            (NodeIndex(3), 1.755917633260777),
+            (NodeIndex(4), 2.6659932095468),
+        ],
+    );
+    check_graph_adjacency(
+        &abstracted_graph,
+        &pickup_node_ids[1],
+        vec![
+            (NodeIndex(0), 1.980472813282169),
+            (NodeIndex(2), 2.4817146592249784),
+            (NodeIndex(3), 1.455229434276506),
+            (NodeIndex(4), 2.0888624885817397),
+        ],
+    );
+    check_graph_adjacency(
+        &abstracted_graph,
+        &pickup_node_ids[2],
+        vec![
+            (NodeIndex(0), 2.075826825805118),
+            (NodeIndex(1), 2.1511220126275212),
+            (NodeIndex(3), 1.8695162433680703),
+            (NodeIndex(4), 2.4192915795174095),
+        ],
+    );
+    check_graph_adjacency(
+        &abstracted_graph,
+        &pickup_node_ids[3],
+        vec![
+            (NodeIndex(0), 7.835010613747839),
+            (NodeIndex(1), 7.604014959490413),
+            (NodeIndex(2), 8.336252459690652),
+            (NodeIndex(4), 6.42873712450868),
+        ],
+    );
+    check_graph_adjacency(
+        &abstracted_graph,
+        &pickup_node_ids[4],
+        vec![
+            (NodeIndex(0), 3.3510388777275084),
+            (NodeIndex(1), 3.0842902626239828),
+            (NodeIndex(2), 3.850361636228818),
+            (NodeIndex(3), 2.921153828236195),
+        ],
+    );
 
     // output_graph_to_file(
     //     &abstracted_graph,
