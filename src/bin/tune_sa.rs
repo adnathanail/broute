498a09b22ce458e92a6b5ea480f508b01348e493
Alex Nathanail <adnathanail@hotmail.co.uk>
2023-03-27 19:50:49 +0100

    Functioning version of Naive SA

--- src/bin/tune_sa.rs
+++ src/bin/tune_sa.rs
@@ -5,15 +5,16 @@ use std::time::SystemTime;
 fn main() {
     let dimacs_g = load_tsplib_file("test_data/dimacs_tsp/d1291.tsp", usize::MAX).unwrap();
 
-    for a in [0.9995] {
-        for i in [50, 100, 500, 1000] {
-            println!("100 {a} {i}");
-            let start = SystemTime::now();
-            let mut sa = SimulatedAnnealing::new_with_custom_parameters(&dimacs_g, 100.0, a, i);
-            sa.run();
-            let end = SystemTime::now();
-            let duration = end.duration_since(start).unwrap();
-            println!("\t{} seconds", duration.as_secs());
-        }
+    for a in 0..10 {
+        println!("{a}");
+        let start = SystemTime::now();
+        let mut sa = SimulatedAnnealing::new(&dimacs_g);
+        sa.run();
+
+        let end = SystemTime::now();
+        let duration = end.duration_since(start).unwrap();
+
+        println!("\t{}", sa.get_best_path().get_length_on_graph(&dimacs_g));
+        println!("\t{} seconds", duration.as_secs());
     }
 }
