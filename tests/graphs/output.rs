9d5de7eb6d6ef59bac4bc97e7cc9b3722ba60280
Alex Nathanail <adnathanail@hotmail.co.uk>
2023-04-04 15:23:09 +0100

    Remove unneeded uncovered lines

--- tests/graphs/output.rs
+++ tests/graphs/output.rs
@@ -3,22 +3,52 @@ use broute::graphs::algorithms::ConnectedComponents;
 use broute::graphs::datastructures::{ALDigraph, Digraph, GraphPath, NodeID, NodeIndex};
 use broute::graphs::output::{
     graph_to_graphviz_body, output_graph_to_file, output_graph_to_file_with_path,
-    path_to_graphviz_body,
+    path_to_graphviz_body, to_svg,
 };
 use std::fs;
 
 fn get_test_graph() -> ALDigraph {
     let mut g = ALDigraph::new(5);
 
-    for i in 0..5 {
-        g.mut_nodes_data().add_node_data_by_parts(
-            NodeID(i),
-            LatLng {
-                latitude: 0.0,
-                longitude: 0.0,
-            },
-        );
-    }
+    g.mut_nodes_data().add_node_data_by_parts(
+        NodeID(0),
+        LatLng {
+            latitude: 1.0,
+            longitude: 0.0,
+        },
+    );
+
+    g.mut_nodes_data().add_node_data_by_parts(
+        NodeID(1),
+        LatLng {
+            latitude: 0.0,
+            longitude: 1.0,
+        },
+    );
+
+    g.mut_nodes_data().add_node_data_by_parts(
+        NodeID(2),
+        LatLng {
+            latitude: 2.0,
+            longitude: 1.0,
+        },
+    );
+
+    g.mut_nodes_data().add_node_data_by_parts(
+        NodeID(3),
+        LatLng {
+            latitude: 0.0,
+            longitude: 3.0,
+        },
+    );
+
+    g.mut_nodes_data().add_node_data_by_parts(
+        NodeID(4),
+        LatLng {
+            latitude: 3.0,
+            longitude: 2.0,
+        },
+    );
 
     g.add_edge_by_id(NodeID(1), NodeID(0), 1.0);
     g.add_edge_by_id(NodeID(0), NodeID(2), 1.0);
@@ -92,4 +122,16 @@ fn output_graph_to_file_with_path_test() {
             .unwrap()
             .contains("M140,-81.79C116.08,-89.86 80.87,-104.46 56.12,-116.45")
     );
+}
+
+#[test]
+fn to_svg_test() {
+    to_svg(
+        &get_test_graph(),
+        &get_test_graph_path(),
+        "out/to_svg_test.svg",
+    );
+    assert!(fs::read_to_string("out/to_svg_test.svg")
+        .unwrap()
+        .contains("M33.333332,0 L0,33.333332 L33.333332,66.666664 L33.333332,0 L0,33.333332 L100,0 L66.666664,100 L33.333332,0"));
 }
