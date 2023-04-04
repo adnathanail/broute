be2be95e9bbb29574ca5e49f1ee145f037184209
Alex Nathanail <adnathanail@hotmail.co.uk>
2023-04-04 15:11:35 +0100

    Test graphviz output 2

--- tests/graphs/output.rs
+++ tests/graphs/output.rs
@@ -1,10 +1,13 @@
 use broute::geography::datastructures::LatLng;
 use broute::graphs::algorithms::ConnectedComponents;
 use broute::graphs::datastructures::{ALDigraph, Digraph, GraphPath, NodeID, NodeIndex};
-use broute::graphs::output::{graph_to_graphviz_body, output_graph_to_file, path_to_graphviz_body};
+use broute::graphs::output::{
+    graph_to_graphviz_body, output_graph_to_file, output_graph_to_file_with_path,
+    path_to_graphviz_body,
+};
+use std::fs;
 
-#[test]
-fn xyz_to_graphviz_body_test() {
+fn get_test_graph() -> ALDigraph {
     let mut g = ALDigraph::new(5);
 
     for i in 0..5 {
@@ -23,8 +26,27 @@ fn xyz_to_graphviz_body_test() {
     g.add_edge_by_id(NodeID(0), NodeID(3), 1.0);
     g.add_edge_by_id(NodeID(3), NodeID(4), 1.0);
 
+    g
+}
+
+fn get_test_graph_path() -> GraphPath {
+    GraphPath {
+        path: vec![
+            NodeIndex(1),
+            NodeIndex(0),
+            NodeIndex(2),
+            NodeIndex(1),
+            NodeIndex(0),
+            NodeIndex(3),
+            NodeIndex(4),
+        ],
+    }
+}
+
+#[test]
+fn graph_to_graphviz_body_test() {
     assert_eq!(
-        graph_to_graphviz_body(&g, "black", true).graph_string,
+        graph_to_graphviz_body(&get_test_graph(), "black", true).graph_string,
         r#"0
 1
 2
@@ -37,26 +59,37 @@ fn xyz_to_graphviz_body_test() {
 3 -> 0[color="black",headlabel="1"]
 "#
     );
+}
 
-    let p = GraphPath {
-        path: vec![
-            NodeIndex(1),
-            NodeIndex(0),
-            NodeIndex(2),
-            NodeIndex(1),
-            NodeIndex(0),
-            NodeIndex(3),
-            NodeIndex(4),
-        ],
-    };
-
+#[test]
+fn path_to_graphviz_body_test() {
     assert_eq!(
-        path_to_graphviz_body(&g, &p).graph_string,
+        path_to_graphviz_body(&get_test_graph(), &get_test_graph_path()).graph_string,
         r#"1 -> 0[headlabel="1", color="red"]
 0 -> 2[headlabel="1", color="red"]
 2 -> 1[headlabel="1", color="red"]
 1 -> 0[headlabel="1", color="red"]
 0 -> 3[headlabel="1", color="red"]
 3 -> 4[headlabel="1", color="red"]"#
+    );
+}
+
+#[test]
+fn output_graph_to_file_test() {
+    output_graph_to_file(&get_test_graph(), "out/output_graph_to_file_test.svg");
+    assert!(fs::read_to_string("out/output_graph_to_file_test.svg").unwrap().contains("M161.74,-213.4C172.33,-213.86 181.3,-211.63 181.3,-206.71 181.3,-203.71 177.97,-201.71 172.97,-200.71"));
+}
+
+#[test]
+fn output_graph_to_file_with_path_test() {
+    output_graph_to_file_with_path(
+        &get_test_graph(),
+        &get_test_graph_path(),
+        "out/output_graph_to_file_with_path_test.svg",
+    );
+    assert!(
+        fs::read_to_string("out/output_graph_to_file_with_path_test.svg")
+            .unwrap()
+            .contains("M140,-81.79C116.08,-89.86 80.87,-104.46 56.12,-116.45")
     );
 }
