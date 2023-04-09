a7e371264cb007b9dfc2963e8eec7f7be56f1310
Alex Nathanail <adnathanail@hotmail.co.uk>
2023-04-09 16:55:38 +0200

    Extract out mean and std_dev

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,5 +1,7 @@
 //! Useful utils or data structures that are not specific to OpenStreetMap data
 
 mod priority_queue;
+mod statistics;
 
 pub use self::priority_queue::*;
+pub use self::statistics::*;
