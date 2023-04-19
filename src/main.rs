d1dd88d2010d0b9b72e83c57f3c10d9724a3fe7a
Alex Nathanail <adnathanail@hotmail.co.uk>
2023-04-19 17:02:15 +0100

    Final stats tweaks

--- src/main.rs
+++ src/main.rs
@@ -186,7 +186,7 @@ impl Fairing for CORS {
 }
 
 async fn get_graph() -> ALDigraph {
-    let g = load_pbf_file("test_data/osm/central-london-latest.osm.pbf").unwrap();
+    let g = load_pbf_file("test_data/osm/monaco-latest.osm.pbf").unwrap();
 
     println!("Original graph {:} nodes", g.num_vertices());
 
--- src/utils/test_data.rs
+++ src/utils/test_data.rs
@@ -6,12 +6,8 @@ use rand_pcg::Pcg64Mcg;
 pub fn get_random_lat_lngs(num_lls: usize, rng: &mut Pcg64Mcg) -> Vec<LatLng> {
     (0..num_lls)
         .map(|_| LatLng {
-            latitude: rng.gen_range(51.518898..51.526952),
-            longitude: rng.gen_range(-0.151246..-0.121154),
+            latitude: rng.gen_range(43.7247599..43.7519311),
+            longitude: rng.gen_range(7.4090279..7.4398704),
         })
         .collect()
 }
-
-// Monaco
-// latitude: rng.gen_range(43.7247599..43.7519311),
-// longitude: rng.gen_range(7.4090279..7.4398704),
