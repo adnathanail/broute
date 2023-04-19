223f9a39bc972aa8ae3a1b67278ff60ab3d9bb67
Alex Nathanail <adnathanail@hotmail.co.uk>
2023-04-19 17:02:15 +0100

    Final stats tweaks

--- src/bin/hc_osm_statistics.rs
+++ src/bin/hc_osm_statistics.rs
@@ -7,28 +7,8 @@ use rand::{Rng, SeedableRng};
 use rand_pcg::Pcg64Mcg;
 use std::time::SystemTime;
 
-fn run_full_tsp<T: Digraph>(g: &T, lat_lng_list: Vec<LatLng>) {
-    let node_id_list = g
-        .nodes_data()
-        .get_node_ids_closest_to_lat_lngs(lat_lng_list);
-
-    let abstracted_graph = form_abstracted_graph(g, &node_id_list);
-
-    let best_path = tsp_with_repeats(&abstracted_graph, 5);
-
-    let p_node_ids: Vec<NodeID> = best_path
-        .path
-        .iter()
-        .map(|node_index| {
-            *abstracted_graph
-                .nodes_data()
-                .get_node_id_by_index(node_index)
-        })
-        .collect();
-}
-
 fn main() {
-    let g = load_pbf_file("test_data/osm/central-london-latest.osm.pbf").unwrap();
+    let g = load_pbf_file("test_data/osm/monaco-latest.osm.pbf").unwrap();
 
     println!("Original graph {:} nodes", g.num_vertices());
 
@@ -40,30 +20,56 @@ fn main() {
 
     let mut rng = Pcg64Mcg::from_entropy();
 
-    println!("graph_num_nodes,duration_mean,duration_std_deviation");
+    println!("graph_num_nodes,geocoding_duration_mean,geocoding_duration_std_deviation,abstracted_graph_duration_mean,abstracted_graph_duration_std_deviation,tsp_duration_mean,tsp_duration_std_deviation");
     for num_nodes in [5, 10, 25, 50, 100, 250, 500, 1000] {
-        let mut durations: Vec<f64> = vec![];
+        let mut geocoding_durations: Vec<f64> = vec![];
+        let mut abstracted_graph_durations: Vec<f64> = vec![];
+        let mut tsp_durations: Vec<f64> = vec![];
 
-        const NUM_ITERATIONS: i32 = 5;
+        const NUM_ITERATIONS: i32 = 10;
 
-        for i in 0..NUM_ITERATIONS {
-            println!("{i}");
+        for _ in 0..NUM_ITERATIONS {
             let lat_lng_list = get_random_lat_lngs(num_nodes, &mut rng);
 
-            let start = SystemTime::now();
-
-            run_full_tsp(&c_g, lat_lng_list);
-
+            let geocoding_start = SystemTime::now();
+            let node_id_list = c_g
+                .nodes_data()
+                .get_node_ids_closest_to_lat_lngs(lat_lng_list);
             let end = SystemTime::now();
-            let duration = end.duration_since(start).unwrap();
+            let geocoding_duration = end.duration_since(geocoding_start).unwrap();
+            geocoding_durations.push((geocoding_duration.as_millis() as f64) / 1000.0);
 
-            durations.push((duration.as_millis() as f64) / 1000.0);
+            let abstracted_graph_start = SystemTime::now();
+            let abstracted_graph = form_abstracted_graph(&c_g, &node_id_list);
+            let end = SystemTime::now();
+            let abstracted_graph_duration = end.duration_since(abstracted_graph_start).unwrap();
+            abstracted_graph_durations
+                .push((abstracted_graph_duration.as_millis() as f64) / 1000.0);
+
+            let tsp_durations_start = SystemTime::now();
+            let best_path = tsp_with_repeats(&abstracted_graph, 5);
+            let p_node_ids: Vec<NodeID> = best_path
+                .path
+                .iter()
+                .map(|node_index| {
+                    *abstracted_graph
+                        .nodes_data()
+                        .get_node_id_by_index(node_index)
+                })
+                .collect();
+            let end = SystemTime::now();
+            let tsp_duration = end.duration_since(tsp_durations_start).unwrap();
+            tsp_durations.push((tsp_duration.as_millis() as f64) / 1000.0);
         }
         println!(
-            "{},{},{}",
+            "{},{},{},{},{},{},{}",
             num_nodes,
-            mean(&durations),
-            std_deviation(&durations),
+            mean(&geocoding_durations),
+            std_deviation(&geocoding_durations),
+            mean(&abstracted_graph_durations),
+            std_deviation(&abstracted_graph_durations),
+            mean(&tsp_durations),
+            std_deviation(&tsp_durations),
         );
     }
 }
